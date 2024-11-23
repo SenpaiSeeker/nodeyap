d22b3a489e60ed52c2a7ae659cb8c3e0967d4987
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-11-24 02:26:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ user_agent = UserAgent(os='windows', platforms='pc', browsers='chrome')
 DOMAIN_API = {
     "SESSION": "https://api.nodepay.ai/api/auth/session",
     "PING": [
-        "http://52.77.10.116/api/network/ping",
         "http://13.215.134.222/api/network/ping",
+        "http://18.139.20.49/api/network/ping",
+        "http://18.142.29.174/api/network/ping",
+        "http://18.142.214.13/api/network/ping",
+        "http://52.74.31.107/api/network/ping",
+        "http://52.74.35.173/api/network/ping",
+        "http://52.77.10.116/api/network/ping",
+        "http://3.1.154.253/api/network/ping"
     ]
 }
 
