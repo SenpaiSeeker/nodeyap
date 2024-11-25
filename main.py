7e4e6e6a3dc096eb41856d7c88bd04778f43c5c7
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-11-25 10:38:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -112,8 +112,7 @@ async def render_profile_info(proxy, token_info):
 
 async def start_ping(proxy, token_info):
     try:
-        while True:
-            await ping(proxy, token_info)
+        await ping(proxy, token_info)
     except asyncio.CancelledError:
         logger.info(f"Ping task for proxy {proxy} was cancelled")
     except Exception as e:
